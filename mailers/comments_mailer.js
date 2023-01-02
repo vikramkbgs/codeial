6c0de4c61951dc83c98b9c430aecc9b8b4b1e59a
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-01-02 11:49:28 +0530

    cloning project in different repo

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,22 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+// this is another way of exporting a method
+exports.newComment = (comment) => {
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+       from: 'vikramkbgs@gmail.com',
+       to: 'vikramroy.sce.sasaram@gmail.com',
+       subject: "New Comment Published!",
+       html: '<h1> Your comments is publised </h1>'
+    }, (err, info) => {
+        if (err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+
+        console.log('Message sent', info);
+        return;
+    });
+}
